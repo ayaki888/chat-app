456196ed8a97e9cbed695d67140b2183a0e53942
ayaki888 <mochida@cross-s.com>
2022-11-09 17:49:01 +0900

    room_usersテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_09_022506) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_09_065252) do
+  create_table "room_users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "rooms", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
